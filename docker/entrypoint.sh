6ef9af7bbd2d92db10b75fed364bd3aa9cd70e4e
Marat Budkevich <93652988+marat2509@users.noreply.github.com>
2024-04-30 23:32:51 +0000

    Push

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,14 +2,8 @@
 
 if [ ! -e "/app/Hikka" ] ; then
     cd /app
-    git clone https://github.com/hikariatama/Hikka -b $BRANCH --depth=1
+    git clone https://github.com/hikariatama/Hikka -b $BRANCH --depth=1 /app
 fi
 
-if [ ! -e "/app/venv" ] ; then
-    virtualenv /app/venv
-fi
-
-source /app/venv/bin/activate
-
-cd /app/Hikka
 python -m hikka
+read -p
